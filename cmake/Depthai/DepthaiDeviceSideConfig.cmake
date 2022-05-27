62a05c6cee81304c5a4a668e00149b3610de5671
alex-luxonis <alex@luxonis.com>
2022-05-27 03:31:14 +0200

    Update FW: ImageManip 4K output, AR0234 trigger initial support (only first trigger working for now, to be fixed)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d6328341176398755600a289c03069343460b358")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "22aabc8af701f38ca0d771e1808e33340bcf1f17")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
