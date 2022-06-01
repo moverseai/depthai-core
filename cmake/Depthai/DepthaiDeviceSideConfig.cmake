e8cd40a22c614e2902f23ea7c1c4b06b353f717a
alex-luxonis <alex@luxonis.com>
2022-06-01 17:15:39 +0300

    Update FW: fix AR0234 trigger settings, usage: - `cam[c].initialControl.setExternalTrigger(1, 0)` for snapshot mode (exposure+readout following immediately after trigger) - `cam[c].initialControl.setFrameSyncMode(dai.CameraControl.FrameSyncMode.INPUT)` for multiple devices sync (frame time is aligned, exposure could be different per device)
    Note: sensor trigger pin reacts on rising edge signal, and keeping the line high will cause repeated triggering.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "22aabc8af701f38ca0d771e1808e33340bcf1f17")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "37340069c4d99425670ed736b845b4481247631c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
