2b7cac3595e1b446e2ee5f9ce2367d0766607ea4
alex-luxonis <alex@luxonis.com>
2022-06-23 09:02:36 +0300

    FW: L/R ports switched to 2-lane

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8ab2dadae5da20a4974737cbddc4fdb944c2327c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7e685e1b0a5bd47b470ab846c65df281d16e9f9a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
