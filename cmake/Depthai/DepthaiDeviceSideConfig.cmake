03a037c9187a52440a308be18f2b1b475f0c836e
alex-luxonis <alex@luxonis.com>
2022-08-20 03:01:39 +0300

    FW: revert ImageManip refactor, ColorCamera::isp processing would be grayscale

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8a763b206e139fb52376c7f2a2f6adbc0e027a83")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a3f1f8ce162d1747bf4f40e7cc41b7de55de7fd9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
