1ce1aba049dfae230f6958d2e46db4cb2b9be9c3
alex-luxonis <alex@luxonis.com>
2022-08-23 03:06:21 +0300

    FW: IMX582 48MP config: 6.44 -> 10 fps max

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a3f1f8ce162d1747bf4f40e7cc41b7de55de7fd9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "69f2bdde634205175eb5646ce6a0754cfa3f08a9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
