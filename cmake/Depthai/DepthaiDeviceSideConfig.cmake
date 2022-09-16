15e3b0106dc1a2131772dffcd8428207a4e76edc
alex-luxonis <alex@luxonis.com>
2022-09-16 14:33:14 +0300

    Support for OAK-D-SR - Left/Right mono

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2470bb3fb3855de1808cf3cc94509ee771bae7d5")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "407003b6d6290920b7c62fa1d1f27632f4b5c039")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
