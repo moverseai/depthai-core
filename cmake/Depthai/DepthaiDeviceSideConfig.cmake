64965631a67b9d8324241d9840ef39ef3e56bbc1
Martin Peterlin <martin.peterlin7@gmail.com>
2022-10-21 18:16:51 +0200

    stability test 2

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a555263a9dbff1166088bcb758ab4306dca9ae1a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "812a21e85fa817f0b192a9219f4752346f058b2e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
